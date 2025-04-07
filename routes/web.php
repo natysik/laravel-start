3a3d0accd00d38c809e85dca64c4bbc451ca6cf0
unknown <natashkakkk@yandex.by>
2025-04-07 21:36:22 +0300

    Create new routes; create new controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-place', 'MyPlaceController@index');
+
+Route::get('/test', 'TestController@getTestName');
