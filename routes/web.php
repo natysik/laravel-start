540bf01e99660672d347dfc479d2335dc3035edf
unknown <natashkakkk@yandex.by>
2025-04-16 20:05:34 +0300

    vies: using yield, section, route

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,13 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
-Route::get('/posts', 'PostController@index');
+Route::get('/posts', 'PostController@index')->name('post.index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/update', 'PostController@update');
 Route::get('/posts/delete', 'PostController@delete');
 Route::get('/posts/first-or-create', 'PostController@firstOrCreate');
 Route::get('/posts/update-or-create', 'PostController@updateOrCreate');
+
+Route::get('/main', 'MainController@index')->name('main.index');
+Route::get('/about', 'AboutController@index')->name('about.index');
+Route::get('/contacts', 'ContactsController@index')->name('contact.index');
