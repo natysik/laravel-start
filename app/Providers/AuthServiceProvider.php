4f7edfdf6d7683de42c78ab7adc306dc2fd473a5
nkozak <natashkakkk@yandex.by>
2025-05-08 13:45:41 +0300

    add policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,29 +2,32 @@
 
 namespace App\Providers;
 
+use App\Policies\AdminPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array<class-string, class-string>
+	 */
+	protected $policies = [
+		// 'App\Models\Model' => 'App\Policies\ModelPolicy',
+		User::class => AdminPolicy::class,
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+	/**
+	 * Register any authentication / authorization services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerPolicies();
 
-        //
-    }
+		//
+	}
 }
